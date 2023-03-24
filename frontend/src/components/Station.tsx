c6082ae082a1bd0f46e4e0edac64fd3b04925baa
Buket Karakaş <buketkarakas8@gmail.com>
2023-03-24 15:46:34 +0200

    Implement top stations view

--- frontend/src/components/Station.tsx
+++ frontend/src/components/Station.tsx
@@ -3,8 +3,10 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom";
+import { Grid } from "@mui/material";
 
 import StationService from "../services/stations";
+import TopStationsList from "./TopStationsList";
 
 interface IStation {
     stationid: number,
@@ -25,14 +27,17 @@ interface IStation {
 interface IJourneyStats {
     startingJourneysCount: number,
     endingJourneysCount: number,
-    averageDistance: number
+    averageDistance: number,
+    topDepartureStations: [],
+    topReturnStations: []
 
 }
 
 export default function Station() {
    
     const [station, setStation] = useState<IStation>();
-    const [journeyStats, setJourneyCounts] = useState<IJourneyStats>();
+    const [journeyStats, setJourneyStats] = useState<IJourneyStats>();
+
 
     const {id}:any = useParams();
     
@@ -46,37 +51,48 @@ export default function Station() {
             );
         StationService
             .getJourneyStats(id)
-            .then(journeyCounts => {
-                setJourneyCounts(journeyCounts)
+            .then(journeyStats => {
+                setJourneyStats(journeyStats)
             })
     }, []);
 
     return (
         <div style={{height: "100%",padding:"10%",  backgroundImage: "url(\"https://images.unsplash.com/photo-1511288561564-8fdcce26ad5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80\")"}}>
-            <Card sx={{ minWidth: 275 }}>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        {station ? station.finnishname : "Loading"}
-                    </Typography>
-                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        {station?.swedishname}
-                    </Typography>
-                    <Typography variant="body2">
-                        {station?.finnishaddress} ({station?.swedishaddress})
-                        <br />
-                        {station?.finnishcity} ({station?.swedishcity})
-                        <br />
-                        <br />
-                        <b>Capacity: </b> {station?.capacity}
-                        <br />
-                        <b>Starting Journeys:</b> {journeyStats?.startingJourneysCount}
-                        <br />
-                        <b>Ending Journeys:</b> {journeyStats?.endingJourneysCount}
-                        <br />
-                        <b>Average distance: </b> {Number(journeyStats?.averageDistance).toPrecision(3)} km
-                    </Typography>
-                </CardContent>
-            </Card>
+                <Card sx={{ minWidth: 275 }}>
+                    <CardContent>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4}>
+                                <Typography variant="h5" component="div">
+                                    {station ? station.finnishname : "Loading"}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    {station?.swedishname}
+                                </Typography>
+                                <Typography variant="body2">
+                                    {station?.finnishaddress} ({station?.swedishaddress})
+                                    <br />
+                                    {station?.finnishcity} ({station?.swedishcity})
+                                    <br />
+                                    <br />
+                                    <b>Capacity: </b> {station?.capacity}
+                                    <br />
+                                    <b>Starting Journeys:</b> {journeyStats?.startingJourneysCount}
+                                    <br />
+                                    <b>Ending Journeys:</b> {journeyStats?.endingJourneysCount}
+                                    <br />
+                                    <b>Average distance: </b> {Number(journeyStats?.averageDistance).toPrecision(3)} km
+                                </Typography>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <TopStationsList title="Top Departure Stations" stations = {journeyStats?.topDepartureStations || []}/>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <TopStationsList title="Top Return Stations" stations = {journeyStats?.topReturnStations || []}/>
+                            </Grid>
+                        </Grid>
+                    </CardContent>
+                </Card>
+            
         </div>
     );
 }
